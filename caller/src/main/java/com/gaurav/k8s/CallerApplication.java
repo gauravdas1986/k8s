e3f5e2e2f5783250b8bfe23c4d917a270d13e5de
gauravdas1986 <32699166+gauravdas1986@users.noreply.github.com>
2020-07-29 17:36:32 +0530

    Add files via upload

---
+++ caller/src/main/java/com/gaurav/k8s/CallerApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallerApplication.class, args);
+	}
+
+}
