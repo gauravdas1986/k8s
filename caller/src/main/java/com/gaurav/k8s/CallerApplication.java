01caf252f5c3522e14a2df8c02571b3bb9d95cd2
Gaurav Das <gauravdas.ti@gmail.com>
2020-07-29 20:03:47 +0530

    feign

--- caller/src/main/java/com/gaurav/k8s/CallerApplication.java
+++ caller/src/main/java/com/gaurav/k8s/CallerApplication.java
@@ -2,8 +2,10 @@ package com.gaurav.k8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.gaurav.k8s.proxy")
 public class CallerApplication {
 
 	public static void main(String[] args) {
