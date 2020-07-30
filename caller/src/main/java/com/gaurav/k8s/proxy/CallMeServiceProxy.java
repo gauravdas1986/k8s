e05329b1b1b500717f07676bbe31ab4b1656211a
Gaurav Das <gauravdas.ti@gmail.com>
2020-07-30 13:40:13 +0530

    port

--- caller/src/main/java/com/gaurav/k8s/proxy/CallMeServiceProxy.java
+++ caller/src/main/java/com/gaurav/k8s/proxy/CallMeServiceProxy.java
@@ -3,7 +3,7 @@ package com.gaurav.k8s.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "currency-exchange", url = "$CURRENCY_EXCHANGE_URI")//
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI}:8002")//
 //@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 //@FeignClient(name = "currency-exchange-service")//Kubernetes Service Name
 public interface CallMeServiceProxy {
